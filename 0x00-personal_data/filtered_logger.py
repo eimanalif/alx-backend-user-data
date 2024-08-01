554dc93b4c60e9615b4dde674abf9ddf4fdeeb1a
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-01 12:02:48 +0000

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,12 @@
 """ a function called filter_datum that returns the log message obfuscated"""
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+         fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
     ''' replace log data'''
     pattern = f"({'|'.join(fields)})=([^{separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
