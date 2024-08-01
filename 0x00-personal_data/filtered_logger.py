339a42be8c1bc8912860040f56bcd7f7a2c38080
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-01 12:44:05 +0000

    task 6

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,10 @@ import mysql.connector
 import os
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
 
 def filter_datum(
          fields: List[str], redaction: str, message: str, separator: str,
