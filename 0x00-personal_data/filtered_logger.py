f1d8c271afec4155deed8f26615fffa866b347dd
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-01 12:31:51 +0000

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,7 +29,8 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """formats a LogRecord"""
-        original_message = super(RedactingFormatter, self).format(record)
+        original_message = super(
+            RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
 
 
