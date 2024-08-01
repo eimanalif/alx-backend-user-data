0109f2c36ddd241a1c1c4ecf5df133e98d14facc
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-01 12:18:37 +0000

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,3 +11,19 @@ def filter_datum(
     ''' replace log data'''
     pattern = f"({'|'.join(fields)})=([^{separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
