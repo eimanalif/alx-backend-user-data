b1222ea6cdd354d76cc81bff158586ce88bc8a51
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-01 12:24:14 +0000

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,10 +20,11 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        original_message = super().format(record)
+        """formats a LogRecord"""
+        original_message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
