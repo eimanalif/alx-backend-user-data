0856340cd1ea02673b1f184d2b85bfa99f990b95
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-06 17:47:22 +0000

    03

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,6 @@ from api.v1.views import app_views
 from flask import jsonify, abort
 
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """GET /api/v1/status
