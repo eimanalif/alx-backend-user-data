b6be94d72a9ebae36b9673f41763f6734af1b55c
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-08 06:47:01 +0000

    02

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""User session module"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """User session class"""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Initializes a User session instance """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
