851454d8527cf8ae4a5d76933a638718625ef71b
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-08 07:43:26 +0000

    02

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -31,3 +31,15 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Gets current user from request"""
         return None
+
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+
+        SESSION_NAME = os.getenv("SESSION_NAME")
+
+        cookie = request.cookies.get(SESSION_NAME)
+
+        return cookie
