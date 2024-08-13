ec4fc5b5e63ec4acfa371286f9e36104e2ce920c
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-13 18:29:55 +0000

    task 0

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
         return self.__session
 
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
 
         Args:
