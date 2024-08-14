3edea211b839b9f394fb5950cf86029a7d6ef55d
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-14 11:13:37 +0000

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,8 +8,7 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
