407998fe5c9a33a8367006a41da0f143ec9ce561
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-14 11:21:39 +0000

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
-"""DB module
-"""
+"""User Database Module."""
+
 from sqlalchemy import create_engine, tuple_
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """Class for managing user data in the database."""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new UserDB instance."""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Get the database session."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-
-        Args:
-            email (str): The user's email address
-            hashed_password (str): The user's hashed password
-
-        Returns:
-            User: The newly created User object
-        """
+        """Create a new user in the database."""
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
@@ -50,21 +40,21 @@ class DB:
             new_user = None
         return new_user
 
-    def find_user_by(self, **fillter) -> User:
-        '''find user based on filter'''
+    def find_user_by(self, **filters) -> User:
+        """Find a user in the database based on filters."""
         filter_fields, filter_values = [], []
         for field, value in filters.items():
             if hasattr(User, field):
-                filter_fields.append(getter(User, field))
+                filter_fields.append(getattr(User, field))
                 filter_values.append(value)
             else:
-                raise InvalidRequestError(f"Invalid filter:{field}")
-            user = self._session.quary(User).filter(
-                tuple_(*filter_fields).in_([tuple(filter_values)])
-            ).first()
-            if user is None:
-                raise NoResultFound("User not found.")
-            return user
+                raise InvalidRequestError(f"Invalid filter: {field}")
+        user = self._session.query(User).filter(
+            tuple_(*filter_fields).in_([tuple(filter_values)])
+        ).first()
+        if user is None:
+            raise NoResultFound("User not found.")
+        return user
 
     def update_user(self, user_id: int, **updates) -> None:
         """Update user information in the database."""
